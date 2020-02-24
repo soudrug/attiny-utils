d487d8831f8cba54d926ec5aa4181ef50dcf658b
soudrug <soudrug@gmail.com>
2020-02-24 10:18:08 +0100

    SoftSerial debug, two args print

--- attiny_debug.h
+++ attiny_debug.h
@@ -1,18 +1,29 @@
 #ifndef ATTTINY_DEBUG_H
 #define ATTTINY_DEBUG_H
+#ifdef DEBUG_SOFT
+#include <SoftwareSerial.h>
+#endif
 
-#define DEBUG
-
-#ifdef DEBUG
+#if defined(DEBUG) || defined(DEBUG_SOFT)
+ #ifdef DEBUG_SOFT
+  #define DEBUG_DECLARE(x,y) SoftwareSerial dbgSerial(x, y);
+  #define DEBUG_CONNECT(x)  dbgSerial.begin(x);
+  #define DEBUG_PRINTLN(x)  dbgSerial.println(x)
+  #define DEBUG_PRINT(x)  dbgSerial.print(x)
+  #define DEBUG_PRINT2(x,y)  dbgSerial.print(x,y)  
+ #else
   #define DEBUG_DECLARE(x,y) 
   #define DEBUG_CONNECT(x)  Serial.begin(x);ACSR &=~(1<<ACIE);ACSR |=~(1<<ACD)
   #define DEBUG_PRINTLN(x)  Serial.println(x)
   #define DEBUG_PRINT(x)  Serial.print(x)
+  #define DEBUG_PRINT2(x,y)  Serial.print(x,y)
+ #endif
 #else
   #define DEBUG_DECLARE(x,y)  
   #define DEBUG_CONNECT(x)  
   #define DEBUG_PRINTLN(x)
   #define DEBUG_PRINT(x)
+  #define DEBUG_PRINT2(x,y)
 #endif
 
 #endif //ATTTINY_DEBUG_H
